5fb356badbd38023d30521c7d658e4049e4ce5df
saujanyamirge23 <saujanyamirge@gmail.com>
2025-08-31 10:56:52 +0530

    Your message about the changes

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name = "saujanya";
+const repoCount = 10;
+
+//console.log(name + repoCount + "value");
+console.log(`Hello my name is ${name} and my repo count ${repoCount}`);
+
+
+const gameName = new String('saujanayam')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+
